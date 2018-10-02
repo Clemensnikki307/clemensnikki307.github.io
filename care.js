a042013d06875b4655f477fd49cd91bc71d0da1d
Clemensnikki307 <38336392+Clemensnikki307@users.noreply.github.com>
2018-10-01 20:46:46 -0700

    Chapter 4

---
+++ care.js
@@ -0,0 +1,33 @@
+/*JavaScript 6th Edition
+      Chapter 4
+
+      Author: Nikki Clemens
+      Date:   9-24-18
+
+      Filename: care.js
+
+*/
+
+
+// function to add quiz 
+	function plantChoice() {
+	var checkboxes = document.getElementsByTagName("input");
+	var checkboxChecked = [];
+	
+	//set up for the checkboxes length to be all selecting the "right" answers
+	for(var i = 0; i < checkboxes.length; i++) {
+		if(checkboxes[i].checked && (checkboxes[i].value === "right")) {
+			checkboxChecked.push(checkboxes[i]);
+			}
+	}
+	
+	
+	//if the letter is "a", "c" & "d"
+	if (checkboxChecked.length === 3) {
+		text = "Great job! You're correct!";
+	}
+	// if checked any other options 
+	else {
+		text = "Nope, try again.";}
+	document.getElementById("quiz").innerHTML = text;
+	}
