b4c8a78cc70c950b31b364d91a2501bbda549972
Clemensnikki307 <38336392+Clemensnikki307@users.noreply.github.com>
2018-11-05 23:15:21 -0700

    chapter 8

--- contact.js
+++ contact.js
@@ -10,6 +10,8 @@
 
 "use strict";
 var formValidity = true;
+var contactUsObject = {},
+	contactUsSubmission;
 
 /* validate required fields */
 function validateRequired() {
@@ -41,6 +43,67 @@ function validateRequired() {
       formValidity = false;
    }
 }
-	
+
+// value for Contact Us field
+function createID() {
+   var firstName = document.getElementById("fNameinput");
+   var lastName = document.getElementById("lNameinput");
+   var phone = document.getElementById("cPhoneinput");
+   var email = document.getElementById("cEmailinput");
+   var message = document.getElementsByTagName("cMessageinput");
+   var firstInit;
+   var lastInit;
+   if (firstname.value !== "" && lastName.value !== "" && phone.value !== "") {
+      firstInit = firstName.value.charAt(0).toUpperCase();
+      lastInit = lastName.value.charAt(0).toUpperCase();
+      acctid = firstInit + lastInit + phone.value;
+// contactUsArray = [];
+      contactUsObject = {};
+      for (var i = 0; i < fields.length - 1; i++) {
+// contactUsArray.push(fields[i].value);
+         contactUsObject[fields[i].name] = fields[i].value;
+      }
+   }
+}
+
+function createString() {
+  contactUsSubmission = JSON.stringify(contactUsObject);
+}
+
+// create event listeners
+function createEventListeners() {
+   var firstName = document.getElementById("fNameinput");
+   var lastName = document.getElementById("lNameinput");
+   var phone = document.getElementById("cPhoneinput");
+   var email = document.getElementById("cEmailinput");
+   var message = document.getElementById("cMessageinput");
+   if (firstName.addEventListener) {
+      firstName.addEventListener("change", createID, false); 
+      lastName.addEventListener("change", createID, false); 
+      phone.addEventListener("change", createID, false);
+      email.addEventListener("change", createID, false); 
+     message.addEventListener("change", createID, false);
+   } else if (firstName.attachEvent) {
+      firstName.attachEvent("onchange", createID);
+      lastName.attachEvent("onchange", createID);
+      phone.attachEvent("onchange", createID);
+      email.attachEvent("onchange", createID);
+      message.attachEvent("onchange", createID);
+      
+   }
+   
+   var submit = document.getElementById("submitButton");
+   if (submit.addEventListener) {
+      submit.addEventListener("click", createString, false);
+   } else if (submit.attachEvent) {
+      submit.attachEvent("onclick", createString);
+   }
+}
+
+if (window.addEventListener) {
+   window.addEventListener("load", createEventListeners, false);
+} else if (window.attachEvent) {
+   window.attachEvent("onload", createEventListeners);
+}
 
 	
